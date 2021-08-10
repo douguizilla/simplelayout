f1b6a592b6a54c7a739ffd6ccf9aa7c22b898e3d
douguizilla <douglasgommez@gmail.com>
2021-08-10 15:42:49 -0300

    created ProfileActivity.kt and refactor MainActivity.kt to FeedActivity.kt

--- app/src/main/java/com/odougle/simplelayout/ProfileActivity.kt
+++ app/src/main/java/com/odougle/simplelayout/ProfileActivity.kt
@@ -3,9 +3,9 @@ package com.odougle.simplelayout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_profile)
     }
 }

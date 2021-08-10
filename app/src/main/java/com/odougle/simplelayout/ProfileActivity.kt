a2b1bf660cf7a75640f088ee3dba3e462855eecb
douguizilla <douglasgommez@gmail.com>
2021-08-10 15:48:34 -0300

    set view binding for ProfileActivity.kt

--- app/src/main/java/com/odougle/simplelayout/ProfileActivity.kt
+++ app/src/main/java/com/odougle/simplelayout/ProfileActivity.kt
@@ -2,10 +2,12 @@ package com.odougle.simplelayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.odougle.simplelayout.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+    private var binding: ActivityProfileBinding = ActivityProfileBinding.inflate(layoutInflater)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        setContentView(binding.root)
     }
 }
